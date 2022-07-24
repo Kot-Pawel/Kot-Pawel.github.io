cbcdcae38b4bbd1456e4185588cce1fe562b6121
Kot-Pawel <109867834+Kot-Pawel@users.noreply.github.com>
2022-07-24 20:02:09 +0200

    Created all probability variables

--- scriptsMaster.js
+++ scriptsMaster.js
@@ -1,11 +1,12 @@
 function saveRollProbability(){
     var rend = document.getElementById("rend").value;
     var targetSave = document.getElementById("targetSave").value;
-    var int = Number(targetSave) + Number(rend);
-    var int = 6 - int;
-    var int = int + 1;
-    var int = int / 6;
-    var saveRollProbability = int;
+    //var int = Number(targetSave) + Number(rend);
+    //var int = 6 - int;
+    //var int = int + 1;
+    //var int = int / 6;
+    //var saveRollProbability = int;
+    var saveRollProbability = (6 - (Number(targetSave) + Number(rend)) + 1) / 6;
     document.getElementById("saveRollProbability").innerHTML = "Save probability " + saveRollProbability;
     wardSaveRollProbability();
 }
@@ -14,6 +15,20 @@ function wardSaveRollProbability() {
     var targetWardSave = document.getElementById("targetWardSave").value;
     var wardSaveRollProbability = ((6 - Number(targetWardSave) + 1)) / 6;
     document.getElementById("wardSaveRollProbability").innerHTML = "Ward save probability " + wardSaveRollProbability;
+    toHitProbability();
+}
+
+function toHitProbability() {
+    var attackerToHit = document.getElementById("attackerToHit").value;
+    var toHitProbability = (6 - Number(attackerToHit) + 1) / 6;
+    document.getElementById("toHitProbability").innerHTML = "To hit probability " + toHitProbability;
+    toWoundProbability();
+}
+
+function toWoundProbability() {
+    var attackerToWound = document.getElementById("attackerToWound").value;
+    var toWoundProbability = (6 - Number(attackerToWound) + 1) / 6;
+    document.getElementById("toWoundProbability").innerHTML = "To wound probability" + toWoundProbability;
 }
 
 function calculateDmg()
