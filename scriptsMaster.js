26047699b2c7f12c2cbc4b9efa99d15cb1f45b7d
Kot-Pawel <109867834+Kot-Pawel@users.noreply.github.com>
2022-07-25 20:30:25 +0200

    Added remaining fields for the rest of application. First draft of calculating hits function.

--- scriptsMaster.js
+++ scriptsMaster.js
@@ -1,11 +1,6 @@
-function saveRollProbability(){
+function saveRollProbability() {
     var rend = document.getElementById("rend").value;
     var targetSave = document.getElementById("targetSave").value;
-    //var int = Number(targetSave) + Number(rend);
-    //var int = 6 - int;
-    //var int = int + 1;
-    //var int = int / 6;
-    //var saveRollProbability = int;
     var saveRollProbability = (6 - (Number(targetSave) + Number(rend)) + 1) / 6;
     document.getElementById("saveRollProbability").innerHTML = "Save probability " + saveRollProbability;
     wardSaveRollProbability();
@@ -18,17 +13,28 @@ function wardSaveRollProbability() {
     toHitProbability();
 }
 
+
 function toHitProbability() {
     var attackerToHit = document.getElementById("attackerToHit").value;
-    var toHitProbability = (6 - Number(attackerToHit) + 1) / 6;
-    document.getElementById("toHitProbability").innerHTML = "To hit probability " + toHitProbability;
+    var toHitPobability = (6 - Number(attackerToHit) + 1) / 6;
+    document.getElementById("toHitProbability").innerHTML = toHitProbability;
     toWoundProbability();
 }
+
+
 
 function toWoundProbability() {
     var attackerToWound = document.getElementById("attackerToWound").value;
     var toWoundProbability = (6 - Number(attackerToWound) + 1) / 6;
     document.getElementById("toWoundProbability").innerHTML = "To wound probability" + toWoundProbability;
+    //hits();
+}
+
+
+function hits() {
+    var numberOfAttacks = document.getElementById("numberOfAttacks").value;
+    var hits = Number(numberOfAttacks) * toHitPobability;
+    document.getElementById("hits").value = hits;
 }
 
 function calculateDmg()
---
+++ test.js
@@ -0,0 +1,14 @@
+function inputTestDataIntoVariables() {
+
+    document.getElementById("numberOfAttacks").value = 10;
+    document.getElementById("dmgPerAttack").value = 2;
+    document.getElementById("rend").value = 1;
+    document.getElementById("targetSave").value = 3;
+    document.getElementById("targetWardSave").value = 7;
+    document.getElementById("attackerToHit").value = 4;
+    document.getElementById("mortal6Hit").value = "no";
+    document.getElementById("attackerToWound").value = 4;
+    document.getElementById("mortal6Wound").value = "no";
+    document.getElementById("mortal6WoundContinues").value = "no";
+
+}
