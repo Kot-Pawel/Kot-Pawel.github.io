b89ce0b458774acca7a9b8dec81df085abd74d38
Kot-Pawel <109867834+Kot-Pawel@users.noreply.github.com>
2022-08-11 20:46:11 +0200

    new fields

--- scriptsMaster.js
+++ scriptsMaster.js
@@ -2,14 +2,16 @@ function saveRollProbability() {
     var rend = document.getElementById("rend").value;
     var targetSave = document.getElementById("targetSave").value;
     var saveRollProbability = (6 - (Number(targetSave) + Number(rend)) + 1) / 6;
-    document.getElementById("saveRollProbability").innerHTML = "Save probability " + saveRollProbability;
+    //document.getElementById("saveRollProbability").innerHTML = "Save probability " + saveRollProbability;
+    sessionStorage.setItem("saveRollProbability", saveRollProbability);
     wardSaveRollProbability();
 }
 
 function wardSaveRollProbability() {
     var targetWardSave = document.getElementById("targetWardSave").value;
     var wardSaveRollProbability = ((6 - Number(targetWardSave) + 1)) / 6;
-    document.getElementById("wardSaveRollProbability").innerHTML = "Ward save probability " + wardSaveRollProbability;
+    //document.getElementById("wardSaveRollProbability").innerHTML = "Ward save probability " + wardSaveRollProbability;
+    sessionStorage.setItem("wardSaveRollProbability", wardSaveRollProbability);
     toHitProbability();
 }
 
@@ -17,7 +19,7 @@ function wardSaveRollProbability() {
 function toHitProbability() {
     var attackerToHit = document.getElementById("attackerToHit").value;
     var toHitProbability = (6 - Number(attackerToHit) + 1) / 6;
-    document.getElementById("toHitProbability").innerHTML = toHitProbability;
+    //document.getElementById("toHitProbability").innerHTML = toHitProbability;
     sessionStorage.setItem("toHitProbability", toHitProbability); //save toHitProbability to session storage
     toWoundProbability();
 }
@@ -27,7 +29,8 @@ function toHitProbability() {
 function toWoundProbability() {
     var attackerToWound = document.getElementById("attackerToWound").value;
     var toWoundProbability = (6 - Number(attackerToWound) + 1) / 6;
-    document.getElementById("toWoundProbability").innerHTML = "To wound probability" + toWoundProbability;
+    //document.getElementById("toWoundProbability").innerHTML = "To wound probability" + toWoundProbability;
+    sessionStorage.setItem("toWoundProbability", toWoundProbability);
     hits();
 }
 
@@ -60,15 +63,32 @@ function mortal6hitdmg() {
         var dmgPerAttack = document.getElementById("dmgPerAttack").value;
         var mortal6hitdmg = Math.round((Number(numberOfAttacks) * (1 / 6)) * Number(dmgPerAttack));
         document.getElementById("mortal6hitdmg").value = mortal6hitdmg;
+        sessionStorage.setItem("mortal6hitdmg", mortal6hitdmg);
     }
 
     if (mortal6HitIdv == "no")
     {
         var mortal6hitdmg = 0;
         document.getElementById("mortal6hitdmg").value = mortal6hitdmg;
+        sessionStorage.setItem("mortal6hitdmg", mortal6hitdmg);
     }
+    mortal6wounds();
 }
 
 
+function mortal6wounds() {
 
+    var mortal6wounds = 0;
+    document.getElementById("mortal6wounds").value = mortal6wounds;
+    sessionStorage.setItem("mortal6wounds", mortal6wounds);
+    wounds();
+}
+
+
+function wounds() {
+
+    var wounds = Math.round((Number(sessionStorage.getItem("hits")) - Number(sessionStorage.getItem("mortal6hitdmg"))) * Number(sessionStorage.getItem("toWoundProbability"))) - Number(sessionStorage.getItem("mortal6wounds"));
+    document.getElementById("wounds").value = wounds;
+
+}
 
