307742f0efb023a509023ad5c237887768a440a4
Kot-Pawel <109867834+Kot-Pawel@users.noreply.github.com>
2022-11-06 18:29:22 +0100

    changed name of scoresheet, tried using this to get data from table cell but failed (or any field), created hardcoded VP calculation for first row

--- scoreSheet.js
+++ scoreSheet.js
@@ -16,13 +16,13 @@ function addOneRowP1() {
         newCell.innerHTML = roundNumberCell;
 
         newCell = newRow.insertCell();
-        newCell.innerHTML = "<tr><td><select name='Strategy'><option value='GainingMomentum'>Gaining momentum</option><option value='EyeForAnEye'>Eye for an eye</option><option value='DesecrateTheirLands'>Desecrate their lands</option><option value='ThisOnesMine'>This one's mine</option><option value='HeadToHead'>Head-to-head</option><option value='Outmusclce'>Outmuscle</option><option value='AgainstTheOdds' selected>Against the odds</option><option value='BargeThroughEnemyLines'>Barge through enemy lines</option></select></td></tr>";
+        newCell.innerHTML = "<tr><td><select name='StrategyP1'><option value='GainingMomentum'>Gaining momentum</option><option value='EyeForAnEye'>Eye for an eye</option><option value='DesecrateTheirLands'>Desecrate their lands</option><option value='ThisOnesMine'>This one's mine</option><option value='HeadToHead'>Head-to-head</option><option value='Outmusclce'>Outmuscle</option><option value='AgainstTheOdds' selected>Against the odds</option><option value='BargeThroughEnemyLines'>Barge through enemy lines</option></select></td></tr>";
 
         var newCell = newRow.insertCell();
-        newCell.innerHTML = "<tr><td><input type='text' name='objectivesNumber' onchange='calculateVP(this)'></td></tr>";
+        newCell.innerHTML = "<tr><td><input type='number' name='objectivesNumberP1' id='objectivesNumberP1Id" + roundNumber + "' onchange='calculateVP()'></td></tr>";
 
         newCell = newRow.insertCell();
-        newCell.innerHTML = "<tr><td><input type='text' name='victoryPoints'></td></tr>";
+        newCell.innerHTML = "<tr><td><input type='number' name='victoryPointsP1' id='victoryPointsP1id" + roundNumber + "'></td></tr>";
     }
 
 }
@@ -45,31 +45,46 @@ function addOneRowP2() {
         newCell.innerHTML = roundNumberCell;
 
         newCell = newRow.insertCell();
-        newCell.innerHTML = "<tr><td><select name='Strategy'><option value='GainingMomentum'>Gaining momentum</option><option value='EyeForAnEye'>Eye for an eye</option><option value='DesecrateTheirLands'>Desecrate their lands</option><option value='ThisOnesMine'>This one's mine</option><option value='HeadToHead'>Head-to-head</option><option value='Outmusclce'>Outmuscle</option><option value='AgainstTheOdds' selected>Against the odds</option><option value='BargeThroughEnemyLines'>Barge through enemy lines</option></select></td></tr>";
+        newCell.innerHTML = "<tr><td><select name='StrategyP2'><option value='GainingMomentum'>Gaining momentum</option><option value='EyeForAnEye'>Eye for an eye</option><option value='DesecrateTheirLands'>Desecrate their lands</option><option value='ThisOnesMine'>This one's mine</option><option value='HeadToHead'>Head-to-head</option><option value='Outmusclce'>Outmuscle</option><option value='AgainstTheOdds' selected>Against the odds</option><option value='BargeThroughEnemyLines'>Barge through enemy lines</option></select></td></tr>";
 
         var newCell = newRow.insertCell();
-        newCell.innerHTML = "<tr><td><input type='text' name='objectivesNumber'></td></tr>";
+        newCell.innerHTML = "<tr><td><input type='number' name='objectivesNumberP2'></td></tr>";
 
         newCell = newRow.insertCell();
-        newCell.innerHTML = "<tr><td><input type='text' name='victoryPoints'></td></tr>";
+        newCell.innerHTML = "<tr><td><input type='number' name='victoryPointsP2'></td></tr>";
     }
 
     
 
 }
 
-function calculateVP(x) {
 
-  console.log("Row index is: " + x.rowIndex);
+function calculateVP() {
 
+    var pointsPerObjective = Number(document.getElementById("pointsPerObjectiveId").value);
+    var objectivesHeld = Number(document.getElementById("player1").rows[1].cells[2].children[0].value);
+    var vitoryPoints = pointsPerObjective * objectivesHeld;
+    document.getElementById("victoryPointsP1id1").value = vitoryPoints;
+    
+    
 }
 
-/**
+function clearSessionStorage() {
+    sessionStorage.clear();
+    
+}
 
-current row and cell javascript
 
-If you are using JQuery, use method.index()
+/***
+function nieRozumiemThis() {
 
-var index = $('table tr').index(tr);
 
-**/
+    console.log($("#pointsPerObjectiveId").attr("id"));
+    console.log(this.id);
+    console.log($(this).attr("id"));
+    console.log($(this).get(0).id);
+    console.log($(this)[0].id);
+
+
+}
+ */
