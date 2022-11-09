36fcdf70e1f4aa9d1fbeb53d37c89be572a9dfa1
Kot-Pawel <109867834+Kot-Pawel@users.noreply.github.com>
2022-11-09 21:35:10 +0100

    Added functions to calculate total vicotry points

--- scoreSheet.js
+++ scoreSheet.js
@@ -3,14 +3,14 @@ function addOneRowP1() {
         var newRow = document.getElementById('player1').insertRow();
 
         var currentRow = document.getElementById("player1").rows.length;
-        var roundNumber = currentRow - 1;
-        var roundNumberCell = "<tr><td>R" + roundNumber + "</td></tr >";
+        var roundNumberP1 = currentRow - 1;
+        var roundNumberCell = "<tr><td>R" + roundNumberP1 + "</td></tr >";
 
-    if (roundNumber >= 6) {
+    if (roundNumberP1 >= 6) {
         alert("Max number of rounds reached");
     }
 
-    if (roundNumber < 6) {
+    if (roundNumberP1 < 6) {
 
         var newCell = newRow.insertCell();
         newCell.innerHTML = roundNumberCell;
@@ -19,10 +19,13 @@ function addOneRowP1() {
         newCell.innerHTML = "<tr><td><select name='StrategyP1'><option value='GainingMomentum'>Gaining momentum</option><option value='EyeForAnEye'>Eye for an eye</option><option value='DesecrateTheirLands'>Desecrate their lands</option><option value='ThisOnesMine'>This one's mine</option><option value='HeadToHead'>Head-to-head</option><option value='Outmusclce'>Outmuscle</option><option value='AgainstTheOdds' selected>Against the odds</option><option value='BargeThroughEnemyLines'>Barge through enemy lines</option></select></td></tr>";
 
         var newCell = newRow.insertCell();
-        newCell.innerHTML = "<tr><td><input type='number' name='objectivesNumberP1' id='objectivesNumberP1Id" + roundNumber + "' onchange='calculateVP()'></td></tr>";
+        newCell.innerHTML = "<tr><td><input type='number' name='objectivesNumberP1' id='objectivesNumberP1Id" + roundNumberP1 + "' onchange='calculateVP_Player1()'></td></tr>";
 
         newCell = newRow.insertCell();
-        newCell.innerHTML = "<tr><td><input type='number' name='victoryPointsP1' id='victoryPointsP1id" + roundNumber + "'></td></tr>";
+        newCell.innerHTML = "<tr><td><input type='number' name='victoryPointsP1' id='victoryPointsP1id" + roundNumberP1 + "'></td></tr>";
+
+        sessionStorage.setItem("roundNumberP1", roundNumberP1);
+        
     }
 
 }
@@ -32,14 +35,14 @@ function addOneRowP2() {
     var newRow = document.getElementById('player2').insertRow();
 
     var currentRow = document.getElementById("player2").rows.length;
-    var roundNumber = currentRow - 1;
-    var roundNumberCell = "<tr><td>R" + roundNumber + "</td></tr >";
+    var roundNumberP2 = currentRow - 1;
+    var roundNumberCell = "<tr><td>R" + roundNumberP2 + "</td></tr >";
 
-    if (roundNumber >= 6) {
+    if (roundNumberP2 >= 6) {
         alert("Max number of rounds reached");
     }
 
-    if (roundNumber < 6) {
+    if (roundNumberP2 < 6) {
 
         var newCell = newRow.insertCell();
         newCell.innerHTML = roundNumberCell;
@@ -48,31 +51,114 @@ function addOneRowP2() {
         newCell.innerHTML = "<tr><td><select name='StrategyP2'><option value='GainingMomentum'>Gaining momentum</option><option value='EyeForAnEye'>Eye for an eye</option><option value='DesecrateTheirLands'>Desecrate their lands</option><option value='ThisOnesMine'>This one's mine</option><option value='HeadToHead'>Head-to-head</option><option value='Outmusclce'>Outmuscle</option><option value='AgainstTheOdds' selected>Against the odds</option><option value='BargeThroughEnemyLines'>Barge through enemy lines</option></select></td></tr>";
 
         var newCell = newRow.insertCell();
-        newCell.innerHTML = "<tr><td><input type='number' name='objectivesNumberP2'></td></tr>";
+        newCell.innerHTML = "<tr><td><input type='number' name='objectivesNumberP2' id='objectivesNumberP2Id" + roundNumberP2 + "' onchange='calculateVP_Player2()'></td></tr>";
 
         newCell = newRow.insertCell();
-        newCell.innerHTML = "<tr><td><input type='number' name='victoryPointsP2'></td></tr>";
-    }
+        newCell.innerHTML = "<tr><td><input type='number' name='victoryPointsP2' id='victoryPointsP2id" + roundNumberP2 + "'></td></tr>";
 
-    
+        sessionStorage.setItem("roundNumberP2", roundNumberP2);
+    }
+       
 
 }
 
+function calculateVP_Player1() {
 
-function calculateVP() {
+    var roundNumberP1 = Number(sessionStorage.getItem("roundNumberP1"));
+    console.log(roundNumberP1);
 
     var pointsPerObjective = Number(document.getElementById("pointsPerObjectiveId").value);
-    var objectivesHeld = Number(document.getElementById("player1").rows[1].cells[2].children[0].value);
-    var vitoryPoints = pointsPerObjective * objectivesHeld;
-    document.getElementById("victoryPointsP1id1").value = vitoryPoints;
-    
-    
+
+    if (roundNumberP1 > 0) {
+
+        var objectivesHeld = Number(document.getElementById("player1").rows[1].cells[2].children[0].value);
+        var victoryPoints = pointsPerObjective * objectivesHeld;
+        document.getElementById("victoryPointsP1id1").value = victoryPoints;
+
+    }
+
+    if (roundNumberP1 > 1) {
+             
+        var objectivesHeld = Number(document.getElementById("player1").rows[2].cells[2].children[0].value);
+        var victoryPoints = pointsPerObjective * objectivesHeld;
+        document.getElementById("victoryPointsP1id2").value = victoryPoints;
+
+    }
+
+    if (roundNumberP1 > 2) {
+
+        var objectivesHeld = Number(document.getElementById("player1").rows[3].cells[2].children[0].value);
+        var victoryPoints = pointsPerObjective * objectivesHeld;
+        document.getElementById("victoryPointsP1id3").value = victoryPoints;
+
+    }
+
+    if (roundNumberP1 > 3) {
+
+        var objectivesHeld = Number(document.getElementById("player1").rows[4].cells[2].children[0].value);
+        var victoryPoints = pointsPerObjective * objectivesHeld;
+        document.getElementById("victoryPointsP1id4").value = victoryPoints;
+
+    }
+
+    if (roundNumberP1 > 4) {
+
+        var objectivesHeld = Number(document.getElementById("player1").rows[5].cells[2].children[0].value);
+        var victoryPoints = pointsPerObjective * objectivesHeld;
+        document.getElementById("victoryPointsP1id5").value = victoryPoints;
+
+    }
 }
 
-function clearSessionStorage() {
-    sessionStorage.clear();
-    
+function calculateVP_Player2() {
+
+    var roundNumberP2 = Number(sessionStorage.getItem("roundNumberP2"));
+    console.log(roundNumberP2);
+
+    var pointsPerObjective = Number(document.getElementById("pointsPerObjectiveId").value);
+
+    if (roundNumberP2 > 0) {
+
+        var objectivesHeld = Number(document.getElementById("player2").rows[1].cells[2].children[0].value);
+        var victoryPoints = pointsPerObjective * objectivesHeld;
+        document.getElementById("victoryPointsP2id1").value = victoryPoints;
+
+    }
+
+    if (roundNumberP2 > 1) {
+
+        var objectivesHeld = Number(document.getElementById("player2").rows[2].cells[2].children[0].value);
+        var victoryPoints = pointsPerObjective * objectivesHeld;
+        document.getElementById("victoryPointsP2id2").value = victoryPoints;
+
+    }
+
+    if (roundNumberP2 > 2) {
+
+        var objectivesHeld = Number(document.getElementById("player2").rows[3].cells[2].children[0].value);
+        var victoryPoints = pointsPerObjective * objectivesHeld;
+        document.getElementById("victoryPointsP2id3").value = victoryPoints;
+
+    }
+
+    if (roundNumberP2 > 3) {
+
+        var objectivesHeld = Number(document.getElementById("player2").rows[4].cells[2].children[0].value);
+        var victoryPoints = pointsPerObjective * objectivesHeld;
+        document.getElementById("victoryPointsP2id4").value = victoryPoints;
+
+    }
+
+    if (roundNumberP2 > 4) {
+
+        var objectivesHeld = Number(document.getElementById("player2").rows[5].cells[2].children[0].value);
+        var victoryPoints = pointsPerObjective * objectivesHeld;
+        document.getElementById("victoryPointsP2id5").value = victoryPoints;
+
+    }
 }
+
+
 
 
 /***
