52c1e1a969492eabc28aa8f7d9ef9f48a6da8a93
Kot-Pawel <109867834+Kot-Pawel@users.noreply.github.com>
2022-11-10 22:04:22 +0100

    Added checkbox for strategy, added total score for player one

--- scoreSheet.js
+++ scoreSheet.js
@@ -13,13 +13,16 @@ function addOneRowP1() {
 
     if (roundNumberP1 < 6) {
 
-        var newCell = newRow.insertCell();
+        newCell = newRow.insertCell();
         newCell.innerHTML = roundNumberCell;
 
         newCell = newRow.insertCell();
         newCell.innerHTML = "<tr><td>" + dropdown + "</td></tr>";
 
-        var newCell = newRow.insertCell();
+        newCell = newRow.insertCell();
+        newCell.innerHTML = "<input type='checkbox' id='strategyDoneP1Id" + roundNumberP1 + "'> ";
+
+        newCell = newRow.insertCell();
         newCell.innerHTML = "<tr><td><input type='number' name='objectivesNumberP1' id='objectivesNumberP1Id" + roundNumberP1 + "' onchange='calculateVP_Player1()'></td></tr>";
 
         newCell = newRow.insertCell();
@@ -45,13 +48,16 @@ function addOneRowP2() {
 
     if (roundNumberP2 < 6) {
 
-        var newCell = newRow.insertCell();
+        newCell = newRow.insertCell();
         newCell.innerHTML = roundNumberCell;
 
         newCell = newRow.insertCell();
         newCell.innerHTML = "<tr><td><select name='StrategyP2'><option value='GainingMomentum'>Gaining momentum</option><option value='EyeForAnEye'>Eye for an eye</option><option value='DesecrateTheirLands'>Desecrate their lands</option><option value='ThisOnesMine'>This one's mine</option><option value='HeadToHead'>Head-to-head</option><option value='Outmusclce'>Outmuscle</option><option value='AgainstTheOdds' selected>Against the odds</option><option value='BargeThroughEnemyLines'>Barge through enemy lines</option></select></td></tr>";
 
-        var newCell = newRow.insertCell();
+        newCell = newRow.insertCell();
+        newCell.innerHTML = "<input type='checkbox' id='strategyDoneP2Id" + roundNumberP2 + "'> ";
+
+        newCell = newRow.insertCell();
         newCell.innerHTML = "<tr><td><input type='number' name='objectivesNumberP2' id='objectivesNumberP2Id" + roundNumberP2 + "' onchange='calculateVP_Player2()'></td></tr>";
 
         newCell = newRow.insertCell();
@@ -63,64 +69,101 @@ function addOneRowP2() {
 
 }
 
+
 function calculateVP_Player1() {
 
     var roundNumberP1 = Number(sessionStorage.getItem("roundNumberP1"));
-    console.log(roundNumberP1);
-
     var pointsPerObjective = Number(document.getElementById("pointsPerObjectiveId").value);
+    var totalScoreP1;
 
     if (roundNumberP1 > 0) {
 
-        var objectivesHeld = Number(document.getElementById("player1").rows[1].cells[2].children[0].value);
+        var objectivesHeld = Number(document.getElementById("player1").rows[1].cells[3].children[0].value);
         var victoryPoints = pointsPerObjective * objectivesHeld;
-        document.getElementById("victoryPointsP1id1").value = victoryPoints;
+        var isStrategyDone = document.getElementById("player1").rows[1].cells[2].children[0].checked;
+        
+        if (isStrategyDone == true) {
+            var victoryPoints = victoryPoints + 2;
+        }
 
+        document.getElementById("victoryPointsP1id1").value = victoryPoints;
+        totalScoreP1 = victoryPoints;
+        
     }
 
     if (roundNumberP1 > 1) {
              
-        var objectivesHeld = Number(document.getElementById("player1").rows[2].cells[2].children[0].value);
+        var objectivesHeld = Number(document.getElementById("player1").rows[2].cells[3].children[0].value);
         var victoryPoints = pointsPerObjective * objectivesHeld;
+        var isStrategyDone = document.getElementById("player1").rows[2].cells[2].children[0].checked;
+
+        if (isStrategyDone == true) {
+            var victoryPoints = victoryPoints + 2;
+        }
+
         document.getElementById("victoryPointsP1id2").value = victoryPoints;
+        totalScoreP1 = totalScoreP1 + victoryPoints;
 
     }
 
     if (roundNumberP1 > 2) {
 
-        var objectivesHeld = Number(document.getElementById("player1").rows[3].cells[2].children[0].value);
+        var objectivesHeld = Number(document.getElementById("player1").rows[3].cells[3].children[0].value);
         var victoryPoints = pointsPerObjective * objectivesHeld;
+        var isStrategyDone = document.getElementById("player1").rows[3].cells[2].children[0].checked;
+
+        if (isStrategyDone == true) {
+            var victoryPoints = victoryPoints + 2;
+        }
+
         document.getElementById("victoryPointsP1id3").value = victoryPoints;
+        totalScoreP1 = totalScoreP1 + victoryPoints;
 
     }
 
     if (roundNumberP1 > 3) {
 
-        var objectivesHeld = Number(document.getElementById("player1").rows[4].cells[2].children[0].value);
+        var objectivesHeld = Number(document.getElementById("player1").rows[4].cells[3].children[0].value);
         var victoryPoints = pointsPerObjective * objectivesHeld;
+        var isStrategyDone = document.getElementById("player1").rows[4].cells[2].children[0].checked;
+
+        if (isStrategyDone == true) {
+            var victoryPoints = victoryPoints + 2;
+        }
+
         document.getElementById("victoryPointsP1id4").value = victoryPoints;
+        totalScoreP1 = totalScoreP1 + victoryPoints;
 
     }
 
     if (roundNumberP1 > 4) {
 
-        var objectivesHeld = Number(document.getElementById("player1").rows[5].cells[2].children[0].value);
+        var objectivesHeld = Number(document.getElementById("player1").rows[5].cells[3].children[0].value);
         var victoryPoints = pointsPerObjective * objectivesHeld;
+        var isStrategyDone = document.getElementById("player1").rows[5].cells[2].children[0].checked;
+
+        if (isStrategyDone == true) {
+            var victoryPoints = victoryPoints + 2;
+        }
+
         document.getElementById("victoryPointsP1id5").value = victoryPoints;
+        totalScoreP1 = totalScoreP1 + victoryPoints;
 
     }
+
+    document.getElementById("totalScoreP1").innerHTML = "Total score " + totalScoreP1;
+
 }
 
 function calculateVP_Player2() {
 
     var roundNumberP2 = Number(sessionStorage.getItem("roundNumberP2"));
-    console.log(roundNumberP2);
-
     var pointsPerObjective = Number(document.getElementById("pointsPerObjectiveId").value);
+    
 
     if (roundNumberP2 > 0) {
 
-        var objectivesHeld = Number(document.getElementById("player2").rows[1].cells[2].children[0].value);
+        var objectivesHeld = Number(document.getElementById("player2").rows[1].cells[3].children[0].value);
         var victoryPoints = pointsPerObjective * objectivesHeld;
         document.getElementById("victoryPointsP2id1").value = victoryPoints;
 
@@ -128,7 +171,7 @@ function calculateVP_Player2() {
 
     if (roundNumberP2 > 1) {
 
-        var objectivesHeld = Number(document.getElementById("player2").rows[2].cells[2].children[0].value);
+        var objectivesHeld = Number(document.getElementById("player2").rows[2].cells[3].children[0].value);
         var victoryPoints = pointsPerObjective * objectivesHeld;
         document.getElementById("victoryPointsP2id2").value = victoryPoints;
 
@@ -136,7 +179,7 @@ function calculateVP_Player2() {
 
     if (roundNumberP2 > 2) {
 
-        var objectivesHeld = Number(document.getElementById("player2").rows[3].cells[2].children[0].value);
+        var objectivesHeld = Number(document.getElementById("player2").rows[3].cells[3].children[0].value);
         var victoryPoints = pointsPerObjective * objectivesHeld;
         document.getElementById("victoryPointsP2id3").value = victoryPoints;
 
@@ -144,7 +187,7 @@ function calculateVP_Player2() {
 
     if (roundNumberP2 > 3) {
 
-        var objectivesHeld = Number(document.getElementById("player2").rows[4].cells[2].children[0].value);
+        var objectivesHeld = Number(document.getElementById("player2").rows[4].cells[3].children[0].value);
         var victoryPoints = pointsPerObjective * objectivesHeld;
         document.getElementById("victoryPointsP2id4").value = victoryPoints;
 
@@ -152,7 +195,7 @@ function calculateVP_Player2() {
 
     if (roundNumberP2 > 4) {
 
-        var objectivesHeld = Number(document.getElementById("player2").rows[5].cells[2].children[0].value);
+        var objectivesHeld = Number(document.getElementById("player2").rows[5].cells[3].children[0].value);
         var victoryPoints = pointsPerObjective * objectivesHeld;
         document.getElementById("victoryPointsP2id5").value = victoryPoints;
 
