a8eca5de7efa99a54615b694fd3fbbe3667acf05
Kot-Pawel <109867834+Kot-Pawel@users.noreply.github.com>
2023-06-25 19:01:34 +0200

    fixing stuff

--- getDataFromJson.js
+++ getDataFromJson.js
@@ -3,19 +3,20 @@ function fetchData() {
         .then(response => response.json())
         .then(data => {
             
-            //const strategyNameInput = document.getElementById('StrategyName');
-            //strategyNameInput.value = data.strategy_name;
-
-            var test = data.strategy_name;
-            document.getElementById('StrategyName').value = test;
-
-            //const strategyKeyInput = document.getElementById('StrategyKey');
-            //strategyKeyInput.value = data.strategy_name;
-
-            console.log(data);
             
+
+            const strategyNameInput = document.getElementById('StrategyName');
+            strategyNameInput.value = data.strategy_name;
+
+            const strategyKeyInput = document.getElementById('StrategyKey');
+            strategyKeyInput.value = data.strategy_name;
+                   
+            console.log(data);
+
         })
         .catch(error => {
             console.error('Error:', error);
         });
 }
+
+
