053e128e60cdf1f6781f7dfcd6520820a298d261
Kot-Pawel <109867834+Kot-Pawel@users.noreply.github.com>
2023-06-25 18:11:47 +0200

    test with processing data from json

--- getDataFromJson.js
+++ getDataFromJson.js
@@ -3,6 +3,13 @@ function fetchData() {
         .then(response => response.json())
         .then(data => {
             // Process the data retrieved from the JSON file
+
+            const strategyNameInput = document.getElementById('StrategyName');
+            strategyNameInput.value = data.strategy_name;
+
+            const strategyKeyInput = document.getElementById('StrategyKey');
+            strategyKeyInput.value = data.strategy_name;
+
             console.log(data);
             // Perform any additional operations with the data
         })
