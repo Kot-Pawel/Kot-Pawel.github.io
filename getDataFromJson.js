314152ddea474235effa31875a1a1d3f8a7b5ca3
Kot-Pawel <109867834+Kot-Pawel@users.noreply.github.com>
2023-06-25 19:28:02 +0200

    managed to fix dom error, turns out i used s instead of S

--- getDataFromJson.js
+++ getDataFromJson.js
@@ -1,22 +1,28 @@
 function fetchData() {
+    
     fetch('data.json')
         .then(response => response.json())
         .then(data => {
             
-            
-
-            const strategyNameInput = document.getElementById('StrategyName');
+            const strategyNameInput = document.getElementById('strategyName');
             strategyNameInput.value = data.strategy_name;
 
-            const strategyKeyInput = document.getElementById('StrategyKey');
+            const strategyKeyInput = document.getElementById('strategyKey');
             strategyKeyInput.value = data.strategy_name;
                    
             console.log(data);
 
         })
         .catch(error => {
-            console.error('Error:', error);
-        });
+        console.error('Error:', error);
+       });
+    
 }
 
 
+function testing() {
+
+    document.getElementById('pointsPerObjectiveId').value = 7;
+    document.getElementById('strategyKey').value = "test";
+
+}
